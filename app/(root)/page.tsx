5d6a0d3fa1fa507eb905f16bde0809a0b1b05dc7
Abid Hussain <orasundev@gmail.com>
2025-05-10 22:36:15 -0700

    5-Cart-Completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,6 +10,8 @@ import { useTheme } from "next-themes";
 //import { useEffect, useState } from "react";
 import MenuBar from "@/components/shared/menubar";
 import { Metadata } from "next";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/products/product-list";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -18,10 +20,14 @@ const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const HomePage = async () => {
   await delay(5000);
-
+  //console.log(sampleData);
   return (
     <>
-      <h1>HomePage</h1>
+      <ProductList
+        data={sampleData.products}
+        title="Latest Arrivals."
+        limit={4}
+      />
     </>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "utfs.io", port: "" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com", port: "" },
+      { protocol: "https", hostname: "platform-lookaside.fbsbx.com", port: "" },
+    ],
+  },
+};
 export default nextConfig;

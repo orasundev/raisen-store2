43eef894e78171dee83c7499954ed34a8625bdc1
Abid Hussain <orasundev@gmail.com>
2025-05-11 11:16:45 -0700

    10-Serverless Environment

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,10 @@
 //import { PrismaClient } from '@prisma/client';
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import sampleData from "./sample-data";
+import prisma from "./prisma";
 
 async function main() {
-  const prisma = new PrismaClient();
+  //const prisma = new PrismaClient();
 
   // await prisma.user.deleteMany();
   // await prisma.user.createMany({ data: sampleData.users })
--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -1,10 +1,11 @@
 "use server";
+import prisma from "@/db/prisma";
 import { LATEST_PRODUCT_LIMIT } from "@/lib/constants";
-import { PrismaClient } from "@/lib/generated/prisma/client";
+
 import { convertToPlainObject } from "@/lib/utils";
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
+  //const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
@@ -12,4 +13,11 @@ export async function getLatestProducts() {
   });
   // convert prisma data into plain object
   return convertToPlainObject(data);
+}
+
+// Get single product by id
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({ where: { slug: slug } });
+
+  // convert prisma data into plain object
 }
